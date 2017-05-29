a29cbe4ed6b100b386b0c3f7d60c69b6a001c5fb
Hiromu OCHIAI <otiai10@gmail.com>
2017-05-29 14:34:45 +0900

    Use SQLite Knowledge in default

--- primes/main.go
+++ primes/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"github.com/otiai10/primes"
+	"github.com/otiai10/primes/knowledge"
+)
+
+func init() {
+	k, err := knowledge.NewSQLiteKnowledge(".primes")
+	if err != nil {
+		panic(err)
+	}
+	primes.Ask(k)
+}
+
 func main() {
 	cmd := getCommand()
 	cmd.Prepare()
